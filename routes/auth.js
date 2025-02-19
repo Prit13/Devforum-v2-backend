5b3e4efd5d56314bfd64951d8499c62eebe8b620
Prit13 <beingstarx77@gmail.com>
2025-02-19 16:55:09 +0530

    first update

---
+++ routes/auth.js
@@ -0,0 +1,93 @@
+const express=require('express')
+const router=express.Router()
+const User=require('../models/User')
+const bcrypt=require('bcrypt')
+const jwt=require('jsonwebtoken')
+
+
+//REGISTER- need username, email, password (pass in body as json)
+/*eg. 
+{
+  "username": "Prit",
+  "email": "prit@gmail.com",
+  "password": "123"
+} */
+router.post("/register",async(req,res)=>{
+    try{
+        const {username,email,password}=req.body
+        const salt=await bcrypt.genSalt(10)
+        const hashedPassword=await bcrypt.hashSync(password,salt)
+        const newUser=new User({username,email,password:hashedPassword})
+        const savedUser=await newUser.save()
+        console.log(savedUser);
+        res.status(200).json(savedUser)
+
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
+
+})
+
+
+//LOGIN         need email and password 
+router.post("/login",async (req,res)=>{
+    try{
+        const user=await User.findOne({email:req.body.email})   // search user with that email id
+        // console.log(user)
+        if(!user){
+            return res.status(404).json("User not found!")
+        }
+        const match=await bcrypt.compare(req.body.password,user.password)
+        
+        if(!match){
+            return res.status(401).json("Wrong credentials!")
+        }
+
+        // if the user found with that email and password, generate jwt token  
+        //  payload: id, username, email, secretkey, expiration (optional)
+        const token=jwt.sign({_id:user._id, username:user.username,email:user.email},process.env.SECRET,{expiresIn:"3d"})    // for 3 days, jwt token will be there
+        const {password,...info}=user._doc      // separating password and other info (storing all info in "info" except password)
+        // _doc is an internal property that stores the raw MongoDB dat        
+        
+        res.status(200).json({info,token: token})            // this is token (jwt)
+        // res.json({token,userId: user._id})
+        // res.status(200).json(user);
+
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
+})
+
+
+
+//LOGOUT             nothing require in body
+router.get("/logout",async (req,res)=>{
+    try{
+        res.clearCookie("token",{sameSite:"none",secure:true}).status(200).send("User logged out successfully!")
+
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//REFETCH USER      to avoid automatically logout after refreshing the page
+router.get("/refetch", (req,res)=>{
+    const token=req.cookies.token
+    // console.log(req.cookies.token);
+    jwt.verify(token,process.env.SECRET,{},async (err,data)=>{
+        // console.log("refetch called")
+        // console.log(data);
+
+        if(err){
+            return res.status(404).json(err)
+        }
+        res.status(200).json(data)
+    })
+})
+
+
+
+module.exports=router
---
+++ routes/users.js
@@ -0,0 +1,65 @@
+const express=require('express')
+const router=express.Router()
+const User=require('../models/User')
+const bcrypt=require('bcrypt')
+const Post=require('../models/Post')
+const Comment=require('../models/Comment')
+const verifyToken = require('../verifyToken')
+
+
+//UPDATE        http://localhost:5000/api/users/651fc2d445450fe6ce5d3cf3 i.e _id
+//              need username,password,email in body 
+router.put("/:id",verifyToken,async (req,res)=>{
+    try{
+        // console.log(req.body);
+        
+        if(req.body.password){
+            const salt=await bcrypt.genSalt(10)
+            req.body.password=await bcrypt.hashSync(req.body.password,salt)
+        }
+        const updatedUser=await User.findByIdAndUpdate(req.params.id,{$set:req.body},{new:true})
+        // console.log("Updated User:", updatedUser);
+        res.status(200).json(updatedUser)
+
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
+})
+
+
+//DELETE                 need _id in endpoint of route
+router.delete("/:id",verifyToken,async (req,res)=>{
+    try{
+        
+        const user = await User.findById(req.params.id);
+        const isMatch = await bcrypt.compare(req.body.password, user.password);
+        
+        if (!isMatch) return res.status(400).json("Incorrect password!");
+
+        await User.findByIdAndDelete(req.params.id)
+        await Post.deleteMany({userId:req.params.id})
+        await Comment.deleteMany({userId:req.params.id})
+        res.status(200).json("User has been deleted!")
+
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
+})
+
+
+//GET USER
+router.get("/:id",async (req,res)=>{
+    try{
+        const user=await User.findById(req.params.id)
+        const {password,...info}=user._doc
+        res.status(200).json(info)
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
+})
+
+
+module.exports=router
